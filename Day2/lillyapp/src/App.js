7871baaee29af4f96a7616d7d4842f887099f10f
THILAGAVATHI.K <lillyskct@gmail.com>
2023-12-10 14:44:47 +0530

    made the changes in app.js file

--- Day2/lillyapp/src/App.js
+++ Day2/lillyapp/src/App.js
@@ -1,10 +1,19 @@
-import './Assets/css/style.html'
+
 import './Assets/css/Style.css' 
 import './Assets/css/nav.css'
 //giving the import to know that the css file is been there 
 function App() {
   return(
     <div>
+      <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#services">Services</a>
+    <a href="#contact">Contact</a>
+    <a href="signup.html">Sign Up</a>
+    <a href="login.html">Login</a>
+
+</nav>
        <form class="login-form">
             <h2>Login</h2>
             <label for="username">Username:</label>
