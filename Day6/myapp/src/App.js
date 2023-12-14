94568100caa09cd12656d12c51b2927f2a227923
THILAGAVATHI.K <lillyskct@gmail.com>
2023-12-14 20:20:49 +0530

    DAY6-REACT

--- Day6/myapp/src/App.js
+++ Day6/myapp/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import About from './Pages/About';  // Import the About component
+import Nav from './Components/Nav';
+import { Route, Routes } from 'react-router-dom';  // Import Routes and Route from react-router-dom
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
